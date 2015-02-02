86b120decf25d2b3258ad32a108fb1df1ca428b4
Matt Richmond <mrichmond@wearetmbr.com>
2015-02-02 13:36:37 -0700

    adding a delay for the redirects, so we don't get blacklisted from WPE

--- redirects.js
+++ redirects.js
@@ -1,8 +1,17 @@
 var assert = require('assert'),
   http = require('http');
 
+var delayPerRequest = 1000;
 describe('Redirects', function(){
   var urls = [
+    {
+      from: 'http://brainfarmcinema.com/blog/tag/video/page/2',
+      to: 'http://brainfarmcinema.com/tag/video'
+    },
+    {
+      from: 'http://brainfarmcinema.com/who_we_are.aspx',
+      to: 'http://brainfarmcinema.com/who-we-are/'
+    },
     {
       'from': 'http://brainfarm.wpengine.com/',
       'to': 'http://brainfarmcinema.com/'
@@ -11,26 +20,51 @@ describe('Redirects', function(){
       'from': 'http://brainfarm.wpengine.com',
       'to': 'http://brainfarmcinema.com/'
     },
+    {
+      from: 'http://brainfarmcinema.com/blog/index.php/category/video/',
+      to: 'http://brainfarmcinema.com/category/video'
+    },
+    {
+      from: 'http://brainfarmcinema.com/blog/index.php/category/video/page/3',
+      to: 'http://brainfarmcinema.com/category/video'
+    },
+    {
+      from: 'http://brainfarmcinema.com/blog/category/video/',
+      to: 'http://brainfarmcinema.com/category/video'
+    },
+    {
+      from: 'http://brainfarmcinema.com/blog/index.php/tag/video/',
+      to: 'http://brainfarmcinema.com/tag/video'
+    },
+    {
+      from: 'http://brainfarmcinema.com/blog/tag/video/',
+      to: 'http://brainfarmcinema.com/tag/video'
+    }
   ];
   for (var i = 0; i < urls.length; i++) {
     var from = urls[i].from,
         to = urls[i].to;
-    setupNewTest(from, to);
+    /* stagger our requests -- because their synchronous, they'll
+    all wait {delay} milliseconds before firing again, so we
+    don't need to have a multiplier of milliseconds */
+    setupNewTest(from, to, delayPerRequest);
   }
 });
 
-function setupNewTest(from, to) {
+function setupNewTest(from, to, delay) {
   describe('From: ' + from + ' To: ' + to, function(){
     var redirectTo = null, code = null;
-    before('Get Response', function(done){
-      http.get(from, function(response){
-        redirectTo = response.headers.location || false,
-        code = response.statusCode;
-        done();
-      })
-      .on('error', function(err){
-        done(new Error('WHOOPS, THERE WAS AN ERROR: ' + err.message));
-      });
+    before(function(done){
+      setTimeout(function(done){
+        http.get(from, function(response){
+          redirectTo = response.headers.location || false,
+          code = response.statusCode;
+          done();
+        })
+        .on('error', function(err){
+          done(new Error('WHOOPS, THERE WAS AN ERROR: ' + err.message));
+        });
+      }, delay, done);
     });
     it('should have a 301 response code', function(done){
       if ( code !== 301 ) {
