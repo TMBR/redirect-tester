8b9d11b45d2dd478f1c2ab9f26c41ca4b2ee9365
Matt Richmond <mrichmond@wearetmbr.com>
2015-02-02 11:36:02 -0700

    breaking tests out a little more specifically

--- test.js
+++ test.js
@@ -1,45 +1,57 @@
 var assert = require('assert'),
   http = require('http');
 
-function testUrl(from, to) {
-  it(from + ' should redirect to ' + to, function(done){
-    http
-      .get(from, function(response){
-        var redirectTo = response.headers.location || false,
-        code = response.statusCode;
-        console.log(response.headers.location);
-        if ( code !== 301 ) {
-          done(new Error('Status code was not 301.  Received a ' + response.statusCode + ' instead.'));
-        }
-        else if ( redirectTo === false ) {
-          done(new Error('Location was not in the headers.'));
-        }
-        else if ( redirectTo !== to ) {
-          done(new Error('Redirect location was not as expected.  Expected: ' + to + ' Received: ' + redirectTo ));
-        }
-        else {
-          done();
-        }
-      })
-      .on('error', function(err){
-        done(new Error('WHOOPS, THERE WAS AN ERROR: ' + err.message));
-      });
-  });
-}
 describe('Redirects', function(){
   var urls = [
     {
       'from': 'http://brainfarm.wpengine.com/',
-      'to': 'http://2brainfarmcinema.com/'
+      'to': 'http://brainfarmcinema.com/'
     },
     {
-      'from': 'http://brainfarm.wpengine.com/',
+      'from': 'http://brainfarm.wpengine.com',
       'to': 'http://brainfarmcinema.com/'
     },
   ];
   for (var i = 0; i < urls.length; i++) {
     var from = urls[i].from,
-      to = urls[i].to;
-      testUrl(from, to);
+        to = urls[i].to;
+    setupNewTest(from, to);
   }
 });
+
+function setupNewTest(from, to) {
+  describe('From: ' + from + ' To: ' + to, function(){
+    var redirectTo = null, code = null;
+    before('Get Response', function(done){
+      http.get(from, function(response){
+        redirectTo = response.headers.location || false,
+        code = response.statusCode;
+        done();
+      })
+      .on('error', function(err){
+        done(new Error('WHOOPS, THERE WAS AN ERROR: ' + err.message));
+      });
+    });
+    it('should have a 301 response code', function(done){
+      if ( code !== 301 ) {
+        done(new Error('Status code was not 301.  Received a ' + response.statusCode + ' instead.'));
+      }
+      else {
+        done();
+      }
+    });
+
+    it('should redirect to expected location', function(done){
+      if ( !redirectTo ) {
+        done(new Error('Location was not in the headers.'));
+      }
+      else if ( redirectTo !== to ) {
+        done(new Error('Redirect location was not as expected.  Expected: ' + to + ' Received: ' + redirectTo ));
+      }
+      else {
+        done();
+      }
+    });
+  });
+
+}
